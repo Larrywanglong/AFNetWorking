45c98d2ffc2eb01fd888a036a7e5610a915c5b8f
Larrywanglong <wanglong@wanglongdeMac-mini.local>
2016-03-27 11:37:27 +0800

    AFNetWorking
    网络环境判断

--- AFNetWorking/ViewController.m
+++ AFNetWorking/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+//1.导入框架
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,7 +18,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.view.backgroundColor = [UIColor greenColor];
+
+    //    2.初始化对象
+    //    注意用shareManger来初始化对象
+    AFNetworkReachabilityManager *reachabilityManger = [AFNetworkReachabilityManager sharedManager];
+    
+    //    3.开始监测
+    [reachabilityManger startMonitoring];
+    
+    //    4.获得网络状态
+    [reachabilityManger setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        NSString *statue;
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:{
+                statue = @"未知的网络";
+                break;
+            }
+            case AFNetworkReachabilityStatusNotReachable:{
+                statue = @"无网络";
+                break;
+            }
+                
+            case AFNetworkReachabilityStatusReachableViaWWAN:{
+                statue = @"蜂窝网络";
+                break;
+            }
+                
+            case AFNetworkReachabilityStatusReachableViaWiFi:{
+                statue = @"WIFI的网络";
+                break;
+            }
+                
+            default:
+                break;
+        }
+//        输出网络环境判断的结果
+        NSLog(@"%@",statue);
+    }];
+
 }
 
 - (void)didReceiveMemoryWarning {
